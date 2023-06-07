3787c12d78ac0b4cfe3da2f0d9ed81060686a9d8
Olha <57017694+olhalutaieva@users.noreply.github.com>
2023-06-07 08:36:49 +0200

    Add files via upload

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,1 +1,4 @@
-//hhh
+const Button = ({ color, text, onClick }) => {
+  return <button onClick={onClick} style={{ backgroundColor: color }} className="btn">{text}</button>
+}
+export default Button
